b62fddad38743c4968e96f902196ae6e31a039be
crunstein <cooperun@gmail.com>
2018-05-27 13:23:11 -0700

    player.js displays full info

--- src/player.js
+++ src/player.js
@@ -2,7 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Player = props => 
-    <div> {props.player.name} </div>
+    <div>
+         <ul>
+            <li>{props.player.name} </li>
+            <li>{props.player.city} - {props.player.team} </li>
+            <li> {props.player.position} </li>
+        </ul>
+    </div>
     
 
 Player.propTypes = {
